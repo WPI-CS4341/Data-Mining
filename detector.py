1dbe79440464bc8ebb495e819b6c01b2daadc99f
Tyler Nickerson <nickersoft@gmail.com>
2016-02-05 21:06:44 -0500

    Added feature to count blockable moves

--- detector.py
+++ detector.py
@@ -2,6 +2,7 @@ import numpy as np
 
 PLAYER_1 = 1
 PLAYER_2 = 2
+EMPTY_CELL = 0
 
 
 class Detector(object):
@@ -9,13 +10,26 @@ class Detector(object):
     def __init__(self, board):
         self.board = board
 
+    # PRIVATE METHODS
+
     def __checkForConnect(self, cells, player):
         is_connect = False
         for i in xrange(0, len(cells) - 1):
             if i != len(cells) - 1:
                 if cells[i] == player and cells[i + 1] == player:
-                    is_connect = True
+                    if (i != 0 and cells[i - 1] == EMPTY_CELL) or \
+                       (i + 2 < len(cells) - 1 and cells[i + 2] == EMPTY_CELL):
+                        is_connect = True
         return is_connect
+
+    def __checkForBlockable(self, cells, player):
+        is_blockable = False
+        for i in xrange(0, len(cells) - 1):
+            if i != len(cells) - 1:
+                if cells[i] == player and cells[i + 1] == 0:
+                    if (i + 2 < len(cells) - 1 and cells[i + 2] == player):
+                        is_blockable = True
+        return is_blockable
 
     def __horizontalMoveCount(self, player):
         player_moves = 0
@@ -50,11 +64,47 @@ class Detector(object):
 
         return player_moves
 
+    def __horizontalBlockCount(self, player):
+        player_moves = 0
+        for row in self.board:
+            if self.__checkForBlockable(row, player):
+                player_moves += 1
+        return player_moves
+
+    def __verticalBlockCount(self, player):
+        player_moves = 0
+        for row in self.board.T:
+            if self.__checkForBlockable(row, player):
+                player_moves += 1
+        return player_moves
+
+    def __diagonalBlockCount(self, player, reverse=False):
+        board = np.flipud(self.board) if reverse else self.board
+        i = 0
+        main_diagonal = np.diag(board)
+        player_moves = 0
+
+        while(len(main_diagonal) != 0):
+            diag_a = np.diag(board, i)
+            diag_b = np.diag(board, -i)
+
+            if self.__checkForBlockable(diag_a, player) or \
+               self.__checkForBlockable(diag_b, player):
+                player_moves += 1
+
+            i += 1
+            main_diagonal = np.diag(board, i)
+
+        return player_moves
+
+    # PUBLIC METHODS
+
     def allFeatures(self):
         return {
             'leftCornerPlayer': self.leftCornerPlayer(),
             'centerPlayer': self.centerPlayer(),
-            'openMoveDiff': self.openMoves()
+            'diffMoves': self.diffMoves(),
+            'blockableMoves': self.blockableMoves()
         }
 
     """
@@ -81,7 +131,10 @@ class Detector(object):
                     player2_count += 1
         return PLAYER_1 if player1_count > player2_count else PLAYER_2
 
-    def openMoves(self):
+    """
+    Returns the difference in moves between the two players
+    """
+    def diffMoves(self):
         total_moves = []
 
         for player in [PLAYER_1, PLAYER_2]:
@@ -91,3 +144,17 @@ class Detector(object):
             total_moves.append(player_diagonal_moves + player_horizontal_moves + player_vertical_moves)
 
         return np.amax(total_moves) - np.amin(total_moves)
+
+    """
+    Returns number of moves which have the potential to be blocked (have an open end)
+    """
+    def blockableMoves(self):
+        total_moves = []
+
+        for player in [PLAYER_1, PLAYER_2]:
+            total_moves.append(self.__diagonalBlockCount(player) +
+                               self.__diagonalBlockCount(player, True) +
+                               self.__verticalBlockCount(player) +
+                               self.__horizontalBlockCount(player)
+                               )
+        return np.sum(total_moves)
